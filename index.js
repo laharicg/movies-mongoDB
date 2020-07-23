bdae0cd69c44f7427cdfd2329085d9225c0d76f7
lahari govada <govada.lahari@gmail.com>
2020-07-23 20:34:18 +0530

    commit

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const mongodb =require("mongodb");
+const bodyParser = require("body-parser")
+const url ="mongodb://localhost:27017"
+const app = express();
+const cors = require("cors");
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/movie",async(req,res) => {
+    console.log(req.body)
+    try {
+        let client = await mongodb.connect(url)
+        let db = client.db("node");
+        let data = await db.collection("movies").insertOne(
+            {
+                Movie_name : req.body.Movie_name,
+                Rating:req.body.Rating,
+                Language:req.body.Language,
+                Cast:req.body.Cast
+            }
+        )
+        await client.close()
+    } catch (error) {
+        console.log(error)
+        
+    }
+    res.json({
+        message:"saved data"
+    })
+})
+app.get("/film/:id",async(req,res) => {
+    try {
+        let client = await mongodb.connect(url)
+        let db = client.db("node");
+        let data = await db.collection("movies").findOne({_id : mongodb.ObjectId(req.params.id)})
+        await client.close()
+        res.json( data);
+
+    } 
+    catch (error) {
+        console.log(error)
+    }
+})
+app.get("/All",async(req,res) => {
+    try {
+        let client = await mongodb.connect(url)
+        let db = client.db("node");
+        let data = await db.collection("movies").find().toArray();
+        await client.close()
+        console.log(data)
+        res.json( data);
+
+    } 
+    catch (error) {
+        console.log(error)
+    }
+})
+
+app.get("/search",async(req,res) => {
+    try {
+        let client = await mongodb.connect(url)
+        let db = client.db("node");
+        let data = await db.collection('movies').find({
+            "$text": {
+              "$search": req.body.query
+            }
+          })
+        await client.close()
+        console.log(data)
+        res.json( data);
+
+    } 
+    catch (error) {
+        console.log(error)
+    }
+})
+
+app.listen(3000,function(){
+    console.log("success")
+
+})
